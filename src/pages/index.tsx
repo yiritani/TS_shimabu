3ae36133d9d23aa08fbb8effa8b699c29539a0d7
yiritani <74131902+yiritani@users.noreply.github.com>
2023-02-05 12:02:49 +0900

    :alien:test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,5 @@
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
-import Links from '../../components/Links'
-import {Headline} from '../../components/Headline'
 import Main from '../../components/main'
-
-function tadanoalert(){
-    alert('tadanoalert')
-}
-
 
 export default function Home() {
     return (
@@ -18,7 +10,7 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Main />
+            <Main page={"index"}/>
         </>
     )
 }

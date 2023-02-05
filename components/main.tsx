6c2f9149a3c180899c4c7699992e9792a4f25065
yiritani <74131902+yiritani@users.noreply.github.com>
2023-02-05 14:14:41 +0900

    :alien:test

---
+++ components/main.tsx
@@ -0,0 +1,46 @@
+import styles from '@/styles/Home.module.css'
+import Links from 'components/Links'
+import {Headline} from 'components/Headline'
+
+
+function tadanoalert(){
+    alert('tadanoalert')
+}
+
+interface Props {
+    page: string
+}
+
+export default function Main(props: Props) {
+    return (
+        <>
+
+            <main className={styles.main}>
+
+                <Headline
+                    title={props.page}
+                    arrayObject={[
+                        {
+                            id: 1,
+                            name: 'test'
+                        },
+                        {
+                            id: 2,
+                            name: 'test2'
+                        }
+                    ]}
+                    idList={[1, 2, 3]}
+                    peopleObject={{
+                        id: 1,
+                        name: 'test'
+                    }}
+                    sex
+                    comp={<code className={styles.code}>src/pages/index.tsx</code>}
+                    function={tadanoalert}
+                />
+                <Links />
+
+            </main>
+        </>
+    )
+}

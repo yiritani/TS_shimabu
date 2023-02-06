096f0c0ef20e49c83426c282ddcc408a469af90e
yiritani <74131902+yiritani@users.noreply.github.com>
2023-02-06 21:55:09 +0900

    :alien:test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
-import Head from 'next/head'
 import {Main} from '@/components/main'
 import {Header} from '@/components/Header'
+import React, {useCallback} from "react";
 
 export default function Home() {
+    const clickHandler = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log(e)
+    }, [])
     return (
         <>
             <Header />
+            <button onClick={clickHandler}>ボタン</button>
             <Main page={"index"}/>
         </>
     )

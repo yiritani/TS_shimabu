8a48d8b25ede3905602e9ad9f38dc9994ff89f81
yiritani <74131902+yiritani@users.noreply.github.com>
2023-02-07 22:36:13 +0900

    :alien:test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import {Main} from '@/components/main'
 import {Header} from '@/components/Header'
-import React, {useCallback} from "react";
+import React, {useCallback, useEffect} from "react";
 
 export default function Home() {
+    useEffect(() => {
+        document.body.style.backgroundColor = "green"
+        return () => {
+            document.body.style.backgroundColor = ""
+        }
+    }, [])
+
     const clickHandler = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
         console.log(e)
     }, [])
